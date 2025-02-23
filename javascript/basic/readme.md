d0f0478a1e6fffafde55a4ced8be6b67d93a3ea5
vipin018 <vipinpathak018@gmail.com>
2025-02-23 12:12:18 +0530

    updated

---
+++ javascript/basic/readme.md
@@ -0,0 +1,18 @@
+## Basic JavaScript
+
+### printing messages to the console
+
+- console.log() -> used to print the message to the console
+- console.error() -> used to print the error message to the console
+- console.warn() -> used to print the warning message to the console
+
+### Variable Declaration
+
+- let -> used to declare a variable
+- const -> used to declare a constant
+- var -> used to declare a variable
+
+*que: what is the difference between let and var?*
+
+- let is block scoped and var is function scoped
+- let is mutable and var is mutable
---
+++ javascript/basic/script.js
@@ -0,0 +1,22 @@
+console.log("first") // this is a log message that is used to print the message to the console
+
+console.error("second") // this is an error message that is used to print the error message to the console
+
+console.warn("third") // this is a warning message that is used to print the warning message to the console
+
+
+// Variable Declaration
+
+let a = 10;
+const b = 20;
+var c = 30;
+
+// let and const are block scoped
+// var is function scoped
+
+// what is the difference between let and const?
+// let is mutable and const is immutable
+
+// what is the difference between var and let?
+// var is function scoped and let is block scoped
+
