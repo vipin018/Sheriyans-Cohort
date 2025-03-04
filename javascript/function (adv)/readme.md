9cac695f6956ae8934e14ac4dd7bbe967ac94c14
vipin018 <vipinpathak018@gmail.com>
2025-03-04 13:21:31 +0530

    updated

--- javascript/function (adv)/readme.md
+++ javascript/function (adv)/readme.md
@@ -30,3 +30,32 @@ maths.div(10,20);
 ```
 - in iife we can create private variables and functions which cannot be accessed outside the iife
 
+*que : difference between higher order function and callback function* 
+
+*ans : higher order function is a function which can return a function or accept a function as an argument, while callback function is a function which is passed as an argument to another function and is called inside the function.
+example:
+```
+function a(function1){
+    function1();
+}
+a(function(){
+    console.log("hello");
+});
+```
+above there 
+HOF is -> a
+callback function is -> function(){
+    console.log("hello");
+}
+
+means jo function ko argument jaise pass kre hai usko callback function kehte hai. and jo function jo return kre hai usko higher order function kehte hai.
+
+#### agar ek funcn mei param mei tumne funcn pass kiya toh jo pass kiya woh callback function hota hai aur jisme pass kiya woh higher order function hota hai.
+
+
+## SCOPE
+
+### GLOBAL SCOPE -> a variable declared outside a function is called a global variable and is accessible everywhere in the code.
+
+### LOCAL SCOPE -> a variable declared inside a function is called a local variable and is accessible only inside the function.
+
--- javascript/function (adv)/script.js
+++ javascript/function (adv)/script.js
@@ -43,7 +43,7 @@ console.log(maths.sub(10,20));
 console.log(maths.mul(10,20));
 console.log(maths.div(10,20));
 
-// hof (HIGHER ORDER FUNCTION) -> in this function we can pass function as an argument and return a function
+// hof (HIGHER ORDER FUNCTION) -> in this function we can pass function as an argument and return a function, in simple words HOF is a funcn jo yaa toh return kare funcn yaa toh accept kre funcn in parameter, ya fir dono kre.
 
 function sayHello(){
     return function(){
@@ -58,6 +58,66 @@ function sayHello(){
 }
 sayHello()()()()(); // this is called currying -> when we pass a function as an argument and return a function
 
+    // OR
+
     
+    function a(function1){
+        function1();
+    }
+    a(function(){
+        console.log("hello");
+    })
+
+    // CALLBACK FUNCTION -> function jo bhi pass kre hai ek function ko as an argument usko callback function kehte hai.
+    
+    function a(function1){
+        function1();
+    }
+    a(function(){
+        console.log("hello");
+    })
+    
+// pure and impure function -> a pure function is a function which always returns the same output for the same input and does not have any side effects. while an impure function is a function which may return different output for the same input and may have side effects.
+
+function pure(a,b){
+    return a+b;
+}
+
+console.log(pure(1,2));
+function impure(a,b){
+    return a+b+Math.random();
+}
+
+console.log(impure(1,2));
+
+
+// SCOPE -> scope is the region of the code where a variable is accessible.
+
+// GLOBAL SCOPE -> a variable declared outside a function is called a global variable and is accessible everywhere in the code.
+
+var x = 10;
+function a(){
+    console.log(x);
+}
+a();
+
+// LOCAL SCOPE -> a variable declared inside a function is called a local variable and is accessible only inside the function.  
+
+function a(){
+    var x = 10;
+    console.log(x);
+}
+a();
+
+// closure -> closure is a function that has access to the variables of its parent scope even after the parent function has closed.
+
+function a(){
+    var x = 10;
+    return function b(){
+        console.log(x);
+    }
+}
+a()();
+
 
 
