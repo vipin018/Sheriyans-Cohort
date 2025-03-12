ae274fd719dfa7e51d33520688af8f118e4a7c82
vipin018 <vipinpathak018@gmail.com>
2025-03-12 13:16:45 +0530

    updated

---
+++ DSA/day1/script.js
@@ -0,0 +1,21 @@
+// integers
+let a = 20;
+console.log(a);
+
+// float
+let b = 2.22;
+console.log(b);
+
+// concatenation
+
+let str1 = "hello";
+let str2 = "world";
+let num = 10;
+console.log(str1 + " " + str2);
+console.log(typeof (str1 + num));
+
+// using concatenation print string and int
+
+let num1 = 2;
+let num2 = 3;
+console.log("sum of " + num1 + " and " + num2 + " is : " + (num1 + num2))
