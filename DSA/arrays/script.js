9fe1195d17bda197798d5bb04c2ae6578db595d6
vipin018 <vipinpathak018@gmail.com>
2025-03-31 19:44:39 +0530

    updated

--- DSA/arrays/script.js
+++ DSA/arrays/script.js
@@ -33,7 +33,31 @@ for(let i=0;i<arr.length;i++){
 }
 
 
+// SUM OF THE ARRAY
+let a = [1,2,3,4,5];
 
+let sum =0;
+
+
+for(let i=0;i<a.length;i++){
+    sum += a[i];
+}
+console.log(`sum of the array is ${sum}`);
+
+// MAX and MIN of the array
+
+let max = arr[0];
+let min = arr[0];
+
+for(let i=0;i<arr.length;i++){
+    if(arr[i]>max){
+        max = arr[i];
+    }
+    if(arr[i]<min){
+        min = arr[i];
+    }
+}
+console.log(`max is ${max} and min is ${min}`);
 
 
 
