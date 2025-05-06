9f5cdbec6fc8a8fd5d8149710cebaca3a61657b2
vipin018 <vipinpathak018@gmail.com>
2025-05-06 10:39:29 +0530

    created library

--- FRONTEND/oops/mini-project/script.js
+++ FRONTEND/oops/mini-project/script.js
@@ -1,6 +1,6 @@
 class library{
     constructor(){
-        this.books = [];
+        this.books = []; 
     }
     addBook(book){
         this.books.push(book);
@@ -11,26 +11,26 @@ class library{
         });
     }
 }
+let ternaLibrary = new library();
+ternaLibrary.addBook("Books of Shaders");
+ternaLibrary.addBook("Threejs Journey");
+
 
 class book{
-    constructor(title, author, genre, price){
+    constructor(title,author,price){
         this.title = title;
         this.author = author;
-        this.genre = genre;
         this.price = price;
         this.readStatus = false;
     }
     information(){
-        console.log(`${this.title} is written by ${this.author} belongs to ${this.genre} genre and costs ${this.price} rupees, and you have ${this.readStatus ? "read" : "not read"} this book`);
+        console.log(`${this.title} is written by ${this.author} and costs ${this.price} rupees, and you have ${this.readStatus ? "read" : "not read"} this book`);
+    }
+    read(){
+        this.readStatus = true;
     }
 }
+let book1 = new book("Books of Shaders", "Wawa Sensei", 199);
+let book2 = new book("Threejs Journey", "Bruno Simon", 299);
 
-let book1 = new book("Deathbed", "John Doe", "Fiction", 199);
-let book2 = new book("You Me and the End of the World", "John Doe", "Romance", 299);
-let book3 = new book("The last of Us", "Neil Gaiman", "Horror", 399);
 
-let ternaLibrary = new library();
-ternaLibrary.addBook(book1);
-ternaLibrary.addBook(book2);
-ternaLibrary.addBook(book3);
-ternaLibrary.listBooks();
