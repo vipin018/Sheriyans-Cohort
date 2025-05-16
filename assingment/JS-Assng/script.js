394d7c69ee9b975273249789dc775f8d730eb0d4
Vipin Pathak <126965441+vipin018@users.noreply.github.com>
2025-05-16 22:36:50 +0530

    Update script.js

--- assingment/JS-Assng/script.js
+++ assingment/JS-Assng/script.js
@@ -1,10 +1,9 @@
-/* Q1- *Age Category Message* – Ask the user for their age. If they are
-under 18, print “You are a minor.” If they are between 18 and 60,
-print “You are an adult.” If they are above 60, print “You are a
-senior citizen.” 
+//  Q1- *Age Category Message* – Ask the user for their age. If they are
+// under 18, print “You are a minor.” If they are between 18 and 60,
+// print “You are an adult.” If they are above 60, print “You are a
+// senior citizen.” 
 
 let age = prompt("Enter your age: ")
 
 age < 18 ? console.log("You are a minor.") : age > 18 && age < 60 ? console.log("You're an adult.") : console.log("You're a senior citizen.")
 
-*/
